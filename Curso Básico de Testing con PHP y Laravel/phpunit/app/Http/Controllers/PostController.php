9e48a6111c34789f6e3998f57adc3351fee0e584
Gilberto <Betho6990@gmail.com>
2023-09-02 23:46:44 -0600

    Retaking Testing with PHP and Laravel Course

--- Curso Básico de Testing con PHP y Laravel/phpunit/app/Http/Controllers/PostController.php
+++ Curso Básico de Testing con PHP y Laravel/phpunit/app/Http/Controllers/PostController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function upload(Request $request) {
+        /**
+         * Recibe el request, sube la foto y redirecciona a profile
+         */
         $request->file('photo')->store('profiles');
 
         return redirect('profile');
